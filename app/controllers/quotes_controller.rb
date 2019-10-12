a600d7ed8710a988763c0bea2a81114d79002ba2
Shannon Rachal <srachal674@gmail.com>
2019-10-12 17:37:08 +0000

    Uploading complete program becasue my github file disapeared.

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,28 @@
+class QuotesController < ApplicationController
+  def index
+    @quote = Quote.order("Random()").first
+  end
+  
+  def create
+    @quote = Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid'
+    end  
+    redirect_to root_path
+  end
+
+  def about
+  end
+
+  def show
+    @quote = Quote.where(:id -> params[:id]).first
+    if @quote.blank?
+      render :text => "Not Found", :status => :not_found
+    end
+  
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
+end
